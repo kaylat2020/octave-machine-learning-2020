558f463e761d220c99038002eb1ca4b3f0144c96
kaylat2020 <72246698+kaylat2020@users.noreply.github.com>
2022-03-20 21:42:16 -0500

    Add files via upload

---
+++ phones.m
@@ -0,0 +1,24 @@
+load phones.txt;
+
+x = phones(:,1:5);
+y = phones(:,6);
+m = size(y)(1,1);
+
+xs = (x.-mean(x))./std(x);
+
+ys = y/100;
+
+xs = [ones(m,1) xs];
+theta = zeros(6,1);
+iterations = 3000;
+alpha = 0.3;
+
+J_history = zeros(iterations,1);
+for i = 1:iterations
+theta = theta - (alpha/m) * (((xs*theta)-ys)'*xs)';
+J_history(i) = 1/(2*m) * sum((xs*theta - ys).^2);
+end;
+
+plot(J_history)
+theta = theta * 100
+%Need to plot cost function
---
+++ sat.m
@@ -0,0 +1,24 @@
+load sat.txt;
+
+x = sat(:,1);
+y = sat(:,2);
+m = size(y);
+
+xs = (x-mean(x))/std(x);
+ys = y/1000;
+
+plot(x,y,'x');
+xlabel('SAT score');
+ylabel('Family Income');
+title('Predicted Family Income based on SAT score');
+hold on;
+
+xs = [ones(m,1) x];
+theta = zeros(1,1);
+iterations = 3000;
+alpha = 0.03;
+
+for i = i:iterations
+theta = theta - (alpha/m) * (((x*theta)-y)'*x)';
+end
+theta
